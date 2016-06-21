7c7b50036bbee863076ded5d985eb47aff2f3e0b
César Henrique Bernabé <cesar-hb@hotmail.com>
2016-06-21 14:21:30 -0300

    Escravo separado em threads

---
+++ br/inf/ufes/pp2016_01/SlaveAttacker.java
@@ -0,0 +1,154 @@
+package br.inf.ufes.pp2016_01;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.rmi.server.UnicastRemoteObject;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
+import javax.crypto.Cipher;
+import javax.crypto.spec.SecretKeySpec;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class SlaveAttacker extends Thread {
+
+  private static List<String> dictionary = new ArrayList<>();
+  private long currentIndex;
+
+  private byte[] ciphertext;
+  private byte[] knowntext;
+  private long initialwordindex;
+  private long finalwordindex;
+  private SlaveManager callbackinterface;
+
+  public SlaveAttacker(byte[] ciphertext, byte[] knowntext, long initialwordindex, long finalwordindex, SlaveManager callbackinterface){
+    this.ciphertext = ciphertext;
+    this.knowntext = knowntext;
+    this.initialwordindex = initialwordindex;
+    this.finalwordindex = finalwordindex;
+    this.callbackinterface = callbackinterface;
+  }
+
+
+  /**
+  * **** Getters and Setters *******
+  *
+  */
+  public long getCurrentIndex() {
+    return currentIndex;
+  }
+
+  /**
+  * *** Realiza Leitura do arquivo de dicionario *****
+  */
+  private void readDictionary() {
+    try {
+      BufferedReader br;
+      br = new BufferedReader(new FileReader("dictionary.txt"));
+
+      //palavra lida
+      String word;
+
+      while ((word = br.readLine()) != null) {
+        dictionary.add(word);
+      }
+
+      br.close();
+    } catch (IOException ex) {
+      System.out.println("Escravo " + this.getId() + ": arquivo de dicionário não encontrado.");
+    }
+  }
+
+  /**
+  * Tenta realizar descriptografia dados texto criptografado e chave
+  */
+  private byte[] decrypt(byte[] key) {
+    try {
+      SecretKeySpec keySpec = new SecretKeySpec(key, "Blowfish");
+
+      Cipher cipher = Cipher.getInstance("Blowfish");
+      cipher.init(Cipher.DECRYPT_MODE, keySpec);
+
+      byte[] decrypted = cipher.doFinal(this.ciphertext);
+
+      return decrypted;
+
+    } catch (javax.crypto.BadPaddingException e) {
+      System.out.println("Senha invalida.");
+      return null;
+
+    } catch (Exception e) {
+      System.out.println("Escravo " + this.getId() + ": erro na descriptografia.");
+      return null;
+    }
+  }
+
+  /**
+  * Checa se resultado da criptografia pode ser considerado válido ou não
+  */
+  private boolean checkGuess(byte[] decrypted_message) {
+    String d_message = new String(decrypted_message);
+    String k_text = new String(this.knowntext);
+
+    return d_message.toLowerCase().contains(k_text.toLowerCase());
+  }
+
+  /**
+  * Inicia o sub-ataque
+  */
+  public void startSubAttack() throws RemoteException {
+    String key;
+    byte[] decrypted_message;
+    long begin = (long) initialwordindex;
+    long end = (long) finalwordindex;
+
+    /**
+    * Código que realiza checkpoint
+    * Baseado em: http://www.tutorialspoint.com/java/util/timer_scheduleatfixedrate_delay.htm
+    */
+    Timer scheduler = new Timer();
+    TimerTask checkpointer = new Checkpointer();
+    scheduler.scheduleAtFixedRate(checkpointer, 20000, 20000);
+
+    for (currentIndex = begin; currentIndex <= end; currentIndex += 1) {
+      key = dictionary.get((int)currentIndex);
+      decrypted_message = decrypt(ciphertext, key.getBytes());
+
+      if (checkGuess(decrypted_message, knowntext)) {
+        Guess guess = new Guess();
+        guess.setKey(key);
+        guess.setMessage(decrypted_message);
+        callbackinterface.foundGuess(currentIndex, guess);
+      }
+    }
+  }
+
+  @Override
+  public void run() {
+
+    try {
+      startSubAttack();
+    } catch (RemoteException e) {
+      e.printStackTrace();
+    }
+  }
+
+  //Inner class que realiza checkpoint a cada 20s
+  private class Checkpointer extends TimerTask {
+    @Override
+    public void run() {
+      try {
+        myManager.checkpoint((long) SlaveImpl.this.getCurrentIndex());
+      } catch (RemoteException ex) {
+        Logger.getLogger(SlaveImpl.class.getName()).log(Level.SEVERE, null, ex);
+      }
+    }
+  }
+}
--- br/inf/ufes/pp2016_01/SlaveImpl.java
+++ br/inf/ufes/pp2016_01/SlaveImpl.java
@@ -22,13 +22,12 @@ public class SlaveImpl implements Slave {
     private static List<String> dictionary = new ArrayList<>();
     private String name;
     private int id;
-    private long currentIndex;
 
-    private SlaveManager myManager;
+    private Thread thread;
 
     /**
      * **** Getters and Setters *******
-     * 
+     *
      */
     public int getId() {
         return id;
@@ -54,90 +53,16 @@ public class SlaveImpl implements Slave {
         this.currentIndex = currentIndex;
     }
 
-    /**
-     * *** Realiza Leitura do arquivo de dicionario *****
-     */
-    private void readDictionary() {
-        try {
-            BufferedReader br;
-            br = new BufferedReader(new FileReader("dictionary.txt"));
-
-            //palavra lida
-            String word;
-
-            while ((word = br.readLine()) != null) {
-                dictionary.add(word);
-            }
-
-            br.close();
-        } catch (IOException ex) {
-            System.out.println("Escravo " + this.getId() + ": arquivo de dicionário não encontrado.");
-        }
-    }
-
-    /**
-     * Tenta realizar descriptografia dados texto criptografado e chave
-     */
-    private byte[] decrypt(byte[] ciphertext, byte[] key) {
-        try {
-            SecretKeySpec keySpec = new SecretKeySpec(key, "Blowfish");
-
-            Cipher cipher = Cipher.getInstance("Blowfish");
-            cipher.init(Cipher.DECRYPT_MODE, keySpec);
-
-            byte[] decrypted = cipher.doFinal(ciphertext);
-
-            return decrypted;
-
-        } catch (javax.crypto.BadPaddingException e) {
-            System.out.println("Senha invalida.");
-            return null;
-
-        } catch (Exception e) {
-            System.out.println("Escravo " + this.getId() + ": erro na descriptografia.");
-            return null;
-        }
-    }
-
-    /**
-     * Checa se resultado da criptografia pode ser considerado válido ou não
-     */
-    private boolean checkGuess(byte[] decrypted_message, byte[] knowntext) {
-        String d_message = new String(decrypted_message);
-        String k_text = new String(knowntext);
-
-        return d_message.toLowerCase().contains(k_text.toLowerCase());
-    }
 
     /**
      * Inicia o sub-ataque
      */
     @Override
     public void startSubAttack(byte[] ciphertext, byte[] knowntext, long initialwordindex, long finalwordindex, SlaveManager callbackinterface) throws RemoteException {
-        String key;
-        byte[] decrypted_message;
-        long begin = (long) initialwordindex;
-        long end = (long) finalwordindex;
-
-        /**
-         * Código que realiza checkpoint 
-         * Baseado em: http://www.tutorialspoint.com/java/util/timer_scheduleatfixedrate_delay.htm
-         */
-        Timer scheduler = new Timer();
-        TimerTask checkpointer = new Checkpointer();
-        scheduler.scheduleAtFixedRate(checkpointer, 20000, 20000);
-
-        for (currentIndex = begin; currentIndex <= end; currentIndex += 1) {
-            key = dictionary.get((int)currentIndex);
-            decrypted_message = decrypt(ciphertext, key.getBytes());
-
-            if (checkGuess(decrypted_message, knowntext)) {
-                Guess guess = new Guess();
-                guess.setKey(key);
-                guess.setMessage(decrypted_message);
-                callbackinterface.foundGuess(currentIndex, guess);
-            }
-        }
+        SlaveAttacker exec = new SlaveAttacker(ciphertext, knowntext, initialwordindex, finalwordindex, callbackinterface);
+        Thread thread = new Thread(exec);
+        this.thread = thread;
+        thread.start();
     }
 
     // Desregistra o escravo da lista do Mestre em caso de termino.
@@ -153,8 +78,8 @@ public class SlaveImpl implements Slave {
             }
         });
     }
-    
-    //Procura pelo mestre 
+
+    //Procura pelo mestre
     private Master searchMaster(String masterName){
         try {
             Registry registry = LocateRegistry.getRegistry(masterName);
@@ -165,31 +90,18 @@ public class SlaveImpl implements Slave {
         }
         return null;
     }
-    
+
     //Registre o escravo no mestre a cada 30s
     private void registerSlave(){
         Timer scheduler = new Timer();
         TimerTask masterRegister = new MasterRegister(this);
         scheduler.scheduleAtFixedRate(masterRegister, 30000, 30000);
     }
-    
-    
-    //Inner class que realiza checkpoint a cada 20s
-    private class Checkpointer extends TimerTask {
-        @Override
-        public void run() {
-            try {
-                myManager.checkpoint((long) SlaveImpl.this.getCurrentIndex());
-            } catch (RemoteException ex) {
-                Logger.getLogger(SlaveImpl.class.getName()).log(Level.SEVERE, null, ex);
-            }
-        }
-    }
-    
+
     //Inner class que realiza o registro do mestre a cada 30s
     private class MasterRegister extends TimerTask {
         private final SlaveImpl s;
-        
+
         public MasterRegister(SlaveImpl s){
             this.s = s;
         }
@@ -206,4 +118,46 @@ public class SlaveImpl implements Slave {
             }
         }
     }
+
+    public static void main(String[] args) {
+     /*
+         Cria instância da interface do mestre,
+         essa operação é necessária para que o escravo consiga se
+         registrar na fila gerenciada pelo mestre.
+      */
+     Master mestre;
+
+     //Para execução distribuida: java SlaveImpl IPESCRAVO IPMESTRE
+     //Aqui o escravo recebe sua propria referencia
+     String host = null;
+     if (args.length > 0) {
+         host = args[0];
+     }
+
+     //Escravo recebe referencia para o mestre
+     //if (args.length > 0) {
+     //System.setProperty("java.rmi.server.hostname", args[0]);
+     //}
+
+     try {
+         //Procura Mestre no Registry
+         System.out.println(host);
+         Registry registry = LocateRegistry.getRegistry(host);
+         mestre = (InterfaceMestre) registry.lookup("ReferenciaMestre");
+         SlaveImpl escravo = new SlaveImpl();
+
+         //cria stub do escravo
+         Slave stub = (Slave) UnicastRemoteObject.exportObject(escravo, 0);
+
+         //De acordo com especificação, escravo deve se registrar no menino mestre
+         mestre.incluirFilaEscravos(stub);
+
+         //"Attach" o metodo que executa operacoes necessarias caso o escravo finalize
+         escravo.attachShutDownHook(mestre);
+         escravo.registerSlave();
+
+     } catch (RemoteException | NotBoundException e) {
+         e.printStackTrace();
+     }
+ }
 }
